902ead485e00edc3a011538bf87492654ce291fd
Nikolas Grottendieck <bitbucket@nikolasgrottendieck.com>
2013-10-13 20:42:40 +0200

    First/Last Header for Columns is now rendered, some markup fixes

--- jquery.compTable.js
+++ jquery.compTable.js
@@ -21,25 +21,52 @@
     function renderCompTables() {
         var startTime = new Date().getTime();
 
-        var colCount = settings.columns;
         var tempWrapper = $();
 
-        $(settings.structure).each( function( index ) {
+		// Create all necessary tables
+		$(settings.structure).each( function( index ) {
             var table = $( "<table><caption></caption><tbody></tbody></table>" );
             var tbody = $( "tbody", table );
 
 			table.attr( "id", this.id );
             table.attr( "class", settings.classes );
 
-
 			$("caption",table).html( this.title );
 
-            /*
-             * Table body
-             */
-            $(this.rows).each(function(index) {
+			// Create first header
+			if(0 === index && 'first' === settings.header) {
+
+				var thead = $( "<thead></thead>" );
                 var tr = $( "<tr></tr>" );
-				var label = $( "<th></th>" ).html(this.name);
+                tr.append( $( "<td></td>" ) );
+
+                for(var i = 0; i < settings.columns; i++) {
+                    tr.append( $( "<th></th>" ) );
+                }
+
+                table.find( "caption" ).after( thead.append( tr ) );
+
+			}
+
+			// Create last footer
+			if(( settings.structure.length - 1 ) === index && 'last' === settings.footer) {
+
+				var tfoot = $( "<tfoot></tfoot>" );
+                var tr = $( "<tr></tr>" );
+                tr.append( $( "<td></td>" ) );
+
+                for(var i = 0; i < settings.columns; i++) {
+                    tr.append( $( "<th></th>" ) );
+                }
+
+                table.find( "tbody" ).after( tfoot.append( tr ) );
+
+			}
+
+            // Create table body
+            $( this.rows ).each( function( index ) {
+				var tr = $( "<tr></tr>" );
+				var label = $( "<th></th>" ).html( this.name );
                 var title = this.title.trim();
 
                 if(title.length > 0) {
@@ -48,18 +75,18 @@
 
                 tr.append(label);
 
-                for(var i = 1; i <= colCount; i++) {
-                    tr.append($("<td></td>"));
+                for(var i = 0; i < settings.columns; i++) {
+                    tr.append( $( "<td></td> " ) );
                 }
 
-                tbody.append(tr);
+                tbody.append( tr );
             });
 
-            tempWrapper = tempWrapper.add(table);
+            tempWrapper = tempWrapper.add( table );
         });
 
         var endTime = new Date().getTime();
-        console.log("Table(s) rendered in: " + (endTime - startTime) + "ms");
+        console.log( "Table(s) rendered in: " + ( endTime - startTime ) + "ms" );
 
         return tempWrapper;
     }
@@ -71,7 +98,7 @@
 		var tables = settings.structure;
 
 		// Iterate over the available tables
-		$(tables).each( function( tIndex ) {
+		$( tables ).each( function( tIndex ) {
 
 			// Clear headers and footers
 			$( "#" + tables[ tIndex ].id + " > thead > tr, "+
@@ -79,8 +106,8 @@
 					find( "td:eq(" + colIndex + ")"  ).html( "" );
 
 			// Clear body content
-			$( "#" + tables[ tIndex ].id + " > tbody > tr" ).each(function( dIndex ) {
-				$(this).find( "td:eq(" + colIndex + ")" ).html( "" );
+			$( "#" + tables[ tIndex ].id + " > tbody > tr" ).each( function( dIndex ) {
+				$( this ).find( "td:eq(" + colIndex + ")" ).html( "" );
 			});
 
 		});
@@ -93,22 +120,22 @@
 		var data = settings.content;
 		var tables = settings.structure;
 
-		if(typeof data[ dataId ] !== 'undefined') {
+		if( typeof data[ dataId ] !== "undefined" ) {
 
 			// Clear column of any existing data
-			$().compTable.clearColumn(colIndex);
+			$().compTable.clearColumn( colIndex );
 
 			// iterate over the available tables
-			$(tables).each( function( tIndex ) {
+			$( tables ).each( function( tIndex ) {
 
 				// Set correct header and footer text
 				$( "#" + tables[ tIndex ].id + " > thead > tr, "+
 			       "#" + tables[ tIndex ].id + " > tfoot > tr" ).
-					find( "td:eq(" + colIndex + ")"  ).html( dataId );
+					find( "th:eq(" + colIndex + ")"  ).html( dataId );
 
 				// Fill body with correct content
-				$( "#" + tables[ tIndex ].id + " > tbody > tr" ).each(function( dIndex ) {
-					$(this).
+				$( "#" + tables[ tIndex ].id + " > tbody > tr" ).each( function( dIndex ) {
+					$( this ).
 						find( "td:eq(" + colIndex + ")" ).
 						html( data[ dataId ][ tIndex ][ dIndex ] );
 				});
